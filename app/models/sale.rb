d8dd21b8157d12a091f2fb3d617c720dc6a0e056
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-02-10 01:41:51 +0000

    add logic to determine if there is an active sale to showthe alert

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,15 +1,22 @@
 class Sale < ActiveRecord::Base
 
-  def finished?
-    ends_on < Date.current
-  end
+# define a class method, Active Record scope
+def self.active 
+  where("sales.starts_on <= ? AND sales.ends_on >= ?",
+    Date.current, Date.current).any?
+end
 
-  def upcoming?
-    starts_on > Date.current
-  end
 
-  def active?
-    starts_on < Date.current && ends_on > Date.current
-  end
+def finished?
+  ends_on < Date.current
+end
+
+def upcoming?
+  starts_on > Date.current
+end
+
+def active?
+  starts_on < Date.current && ends_on > Date.current
+end
 
 end
