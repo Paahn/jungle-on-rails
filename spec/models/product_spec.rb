b37ba0a00a6179d7bfa445fcc7bc03e88ec988c7
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-02-11 00:43:35 +0000

    added 5 examples for the product validations

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it 'should validate successfuly if all fields are present'
+
+    it 'should fail validation if name is missing'
+
+    it 'should fail validation if price is missing'
+
+    it 'should fail validation if quantity is missing'
+
+    it 'should fail validation is category is missing'
+    
+  end
 end
