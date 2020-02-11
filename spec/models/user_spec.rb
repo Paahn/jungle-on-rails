e12f73d9b917a8048c45a6281144f8d16a80cbf3
Panos Hatzinikolaou <pjhatzis@gmail.com>
2020-02-11 15:51:55 -0800

    create empty validation stubs for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validation' do
+    it 'should fail validation if there is no email'
+    end
+    it 'should fail validation if there is no first name'
+    end
+    it 'should fail validation if there is no last name'
+    end
+    it 'should validate succesfully with a valid user'
+    end
+    it 'should fail validation when there is no password'
+    end
+    it 'should fail when there is no password confirmation'
+    end
+    it 'should fail when password confirmation doesnt match'
+    end
+    it 'should fail when email already exists (not case sensitive)'
+    end
 end
